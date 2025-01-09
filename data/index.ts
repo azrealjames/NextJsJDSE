c94b0e65649353382dacb44d9d726fa66a2fe88b
azrealjames <azrealjames@gmail.com>
2025-01-08 21:48:57 -0700

    updated links for index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,9 +1,7 @@
-import { title } from "process";
-
 export const navItems = [
   { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  // { name: "Projects", link: "#projects" },
+  // { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
